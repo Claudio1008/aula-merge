2f71867a9c6d464e05315072b4749e3cfeacf4cd
Claudio Gabriel G Batista <claudiogabrielbatista7@gmail.com>
2025-03-07 14:26:38 -0300

    alunoA inserindo linha 3, 4 e 5

--- conflito.ts
+++ conflito.ts
@@ -1,2 +1,5 @@
 console.log(`alunoA alterou a linha 1 do arquivo conflito.ts`);
  console.log(`alunoB alterou a linha 2 do arquivo conflito.ts`);
+
+ let concatenar: string = `Git` + `Hub`;
+console.lo(`Concatenado ${concatenar}`);
