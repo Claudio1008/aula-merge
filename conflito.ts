d9e52749b086fe4f69cf4a80b88fad0f2ee51330
Joao Gabriel Sabino De Araujo <joaogabrielsraujo@gmail.com>
2025-03-07 14:27:21 -0300

    inserindo linha3 em branco e linha 4 criando calculo

--- conflito.ts
+++ conflito.ts
@@ -1,2 +1,5 @@
 console.log(`alunoA alterou a linha 1 do arquivo conflito.ts`);
  console.log(`alunoB alterou a linha 2 do arquivo conflito.ts`);
+
+ let calculo: number = (75 + 75) /3; //imaginando um calculo qualquergit add .
+git commit -m "inserindo linha3 em branco e linha 4 criando calculo"
