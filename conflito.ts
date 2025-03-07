7dee32d46ac70cff935fb7f55ccc7db6e72f9efb
Claudio Gabriel G Batista <claudiogabrielbatista7@gmail.com>
2025-03-07 15:56:52 -0300

    fazendo merge (mesclando as alterações do alunoA e alunoB)

--- conflito.ts
+++ conflito.ts
@@ -1,6 +1,7 @@
 console.log(`alunoA alterou a linha 1 do arquivo conflito.ts`);
  console.log(`alunoB alterou a linha 2 do arquivo conflito.ts`);
 
+ let calculo: number = (75 + 75) /3; //imaginando um calculo qualquer
  let concatenar: string = `Git` + `Hub`;
 console.log(`Concatenado ${concatenar}`);
 
